6214ac0a3b9bb52adeea941fb95c9d2d06f3a621
jetezz <cuadrabc2015@gmail.com>
2026-01-23 16:57:44 +0100

    http

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
+  server: {
+    host: true,
+    port: 3000,
+    https: false // <--- FUERZA HTTP AQUÍ
+  },
   vite: {
     server: {
+      https: false, // <--- DOBLE SEGURIDAD PARA VITE
       watch: {
         usePolling: true,
       }
