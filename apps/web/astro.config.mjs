c70d4eeec3426d6a964ce65c9ab61447f86f1ba2
jetezz <cuadrabc2015@gmail.com>
2026-01-23 17:42:45 +0100

    refactor: migrate to native @astrojs/bun adapter

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import bun from '@astrojs/bun';
 
 // https://astro.build/config
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: bun()
 });
